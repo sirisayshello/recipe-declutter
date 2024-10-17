f65b10340d03c9a1f496c3c6478ebc1e362d61b8
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-10-17 14:50:49 +0200

    add prisma client with a first working query

---
+++ src/app/db-test/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { getRecipes } from "@/lib/queries";
+import { useEffect, useState } from "react";
+
+// This file was used as a way to test that the prisma client works correctly.
+// Including it in the commit for testing and review purposes.
+
+// Add the contents of this file to the "Dashboard" page later if necessary, or delete it
+// (A separate file was created in order to avoid merge conflicts)
+
+export default function DatabaseTest() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      const data = await getRecipes();
+      setRecipes(data);
+    };
+
+    fetchRecipes();
+  }, []);
+
+  return (
+    <div>
+      {recipes.length > 0 ? (
+        recipes.map((recipe) => <div key={recipe.id}>{recipe.title}</div>)
+      ) : (
+        <div>no recipes in db yet</div>
+      )}
+    </div>
+  );
+}

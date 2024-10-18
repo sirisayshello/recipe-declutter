8ac1e9e685137c9f5a8e9fee8d8d2601095eebd0
Julia <jsgoransson@gmail.com>
2024-10-18 10:14:11 +0200

    installed MUI

---
+++ src/app/StyledRoot.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+
+export function StyledRoot({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
---
+++ src/app/theme.ts
@@ -0,0 +1,17 @@
+"use client";
+import { Poppins } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const poppins = Poppins({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: poppins.style.fontFamily,
+  },
+});
+
+export default theme;

2163aaa3cc78916accce6610dbef5e2c77172413
sirisayshello <veckovis.salongen0p@icloud.com>
2024-10-18 11:40:35 +0200

    style greybox for sign in and sign up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,3 +1,90 @@
+"use client";
+
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 export default function Signup() {
-  return <p>Sign Up Page</p>
+  const handleSubmit = () => {
+    console.log("sign up");
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 2,
+      }}
+    >
+      <Typography variant="h1">Create account</Typography>
+      <FormControl>
+        <FormLabel htmlFor="name">First and last name</FormLabel>
+        <TextField
+          // error={nameError}
+          // helperText={nameErrorMessage}
+          id="name"
+          type="text"
+          name="name"
+          placeholder="Jane Doe"
+          autoFocus
+          required
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <TextField
+          // error={emailError}
+          // helperText={emailErrorMessage}
+          id="email"
+          type="email"
+          name="email"
+          placeholder="your@email.com"
+          autoFocus
+          required
+        />
+      </FormControl>
+      <FormControl>
+        <TextField
+          // error={passwordError}
+          // helperText={passwordErrorMessage}
+          name="password"
+          placeholder="••••••"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          autoFocus
+          required
+          fullWidth
+          variant="outlined"
+          // color={passwordError ? "error" : "primary"}
+        />
+      </FormControl>
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        // onClick={validateInputs}
+      >
+        Sign in
+      </Button>
+      <Typography sx={{ textAlign: "center" }}>
+        Already have an account?{" "}
+        <span>
+          <Link href="/signin" variant="body2" sx={{ alignSelf: "center" }}>
+            Sign in
+          </Link>
+        </span>
+      </Typography>
+    </Box>
+  );
 }

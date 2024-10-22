811d760dabc7970784f4269efb1376665ced328b
JoarHansson <124060119+JoarHansson@users.noreply.github.com>
2024-10-22 11:55:04 +0200

    update prisma schema and clear old migrations

--- prisma/migrations/20241022093413_edit_models/migration.sql
+++ prisma/migrations/20241022093413_edit_models/migration.sql
@@ -2,16 +2,20 @@
 CREATE TABLE "Recipe" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "userId" INTEGER,
+    "slug" TEXT NOT NULL,
+    "ingredients" TEXT[],
+    "instructions" TEXT[],
+    "userId" TEXT,
 
     CONSTRAINT "Recipe_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );

7ccb8ac6637b2b4c81a7244275839a5244266f5a
Julia <jsgoransson@gmail.com>
2024-10-23 08:43:14 +0200

    improve-login-logic

--- src/app/test-page/page.tsx
+++ src/app/test-page/page.tsx
@@ -6,30 +6,23 @@
 // Pick parts from here and add to login page if needed.
 // Outcommented stuff is kept for better backtracking.
 //
-
-import { Button, TextField } from "@mui/material";
-// import { getServerSession } from "next-auth";
-// import { authOptions } from "@/lib/auth";
-import { signIn, signOut } from "next-auth/react";
+import { TextField, Button, Alert, Box, Typography } from "@mui/material";
+import { signIn, signOut, useSession } from "next-auth/react";
 import { useState } from "react";
-// import { useRouter } from "next/navigation";
-import { test } from "@/lib/actions";
+import { useRouter } from "next/navigation";
 
 export default function TestPage() {
-  // const router = useRouter();
-
-  // const session = await getServerSession(authOptions);
-  // console.log(session);
-
-  // const session = useSession();
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-  const onSubmit = async (/* data: LoginForm */ e: any) => {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    // console.log(session);
+    setError(""); // Reset any errors on submit
 
     const result = await signIn("credentials", {
       redirect: false,
@@ -38,85 +31,87 @@ export default function TestPage() {
     });
 
     if (result?.error) {
+      setError("Invalid email or password. Please try again.");
       console.error("Login failed", result.error);
     } else {
-      // router.push("/");
       console.log("Login successful:", result);
-
-      const test2 = await test();
-
-      console.log("client side log:", { test2 });
+      router.push("/");
     }
   };
 
   return (
-    <main
-      style={{
+    <Box
+      sx={{
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
-        height: "70vh",
+        alignItems: "top",
+        marginTop: "150px",
+        width: "100vw",
       }}
     >
-      <div>
-        <button style={{ marginRight: 10 }} onClick={() => signOut()}>
-          Sign Out
-        </button>
-
-        <form onSubmit={onSubmit}>
-          <div className="flex flex-col items-center gap-4">
-            <div className="my-1 flex flex-col w-full">
-              <label
-                htmlFor="email"
-                className="text-sm font-medium text-gray-700"
-              >
-                Mailadress
-              </label>{" "}
-              <TextField
-                className="w-full"
-                id="outlined-size-small"
-                type="email"
-                size="small"
-                placeholder="exempel@mail.se"
-                value={email}
-                onChange={(event) => setEmail(event?.target.value)}
-              />
-            </div>
-
-            <div className="my-1 flex flex-col w-full">
-              <label
-                htmlFor="password"
-                className="text-sm font-medium text-gray-700"
-              >
-                Lösenord
-              </label>{" "}
-              <TextField
-                className="w-full"
-                id="outlined-size-small"
-                type="password"
-                size="small"
-                placeholder="•••••••••"
-                name=""
-                value={password}
-                onChange={(event) => setPassword(event?.target.value)}
-              />
-            </div>
-
-            <Button type="submit" variant="contained" className="w-full mt-2">
-              Logga in
+      <form onSubmit={onSubmit} className="w-full m-8">
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 3,
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 1,
+              width: "100%",
+            }}
+          >
+            <Typography variant="body1" color="textSecondary" gutterBottom>
+              Mailadress
+            </Typography>
+            <TextField
+              id="outlined-size-small"
+              type="email"
+              size="small"
+              placeholder="exempel@mail.se"
+              value={email}
+              onChange={(event) => setEmail(event?.target.value)}
+            />
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              gap: 1,
+              width: "100%",
+            }}
+          >
+            <Typography variant="body1" color="textSecondary" gutterBottom>
+              Lösenord
+            </Typography>
+            <TextField
+              className="w-full"
+              id="outlined-size-small"
+              type="password"
+              size="small"
+              placeholder="•••••••••"
+              name=""
+              value={password}
+              onChange={(event) => setPassword(event?.target.value)}
+            />
+          </Box>
+          {error && <Alert severity="error">{error}</Alert>}{" "}
+          {/* Display error message */}
+          <Button type="submit" variant="contained" className="w-full mt-2">
+            Logga in
+          </Button>
+          {session && (
+            <Button variant="outlined" onClick={() => signOut()}>
+              Sign Out
             </Button>
-            <p>
-              Har du inget konto?{" "}
-              <a href="/register" className="underline underline-offset-3">
-                Skapa konto
-              </a>
-            </p>
-          </div>
-        </form>
-
-        <h1>Server Session</h1>
-        {/* <pre>{JSON.stringify(session)}</pre> */}
-      </div>
-    </main>
+          )}
+        </Box>
+      </form>
+    </Box>
   );
 }

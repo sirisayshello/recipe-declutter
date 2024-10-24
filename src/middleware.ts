073f59252fbc1f905e671544fa690f145f17384b
Julia <142974149+juliasophieg@users.noreply.github.com>
2024-10-24 13:28:09 +0200

    feat: add authorization check for pages (#55)
    * added middleware to protect all /dashboard paths
    
    * add redirects for authorized users

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+// getServerSession can not be used in middleware. Checking for user token instead.
+
+export async function middleware(request: NextRequest) {
+  // Check for session token
+  const token = await getToken({
+    req: request,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  // Redirect to login page if there's no token i.e. user is not authenticated
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // Redirect authenticated users away from /login and /signup
+  if (token) {
+    const { pathname } = request.nextUrl;
+    if (pathname === "/login" || pathname === "/signup") {
+      return NextResponse.redirect(new URL("/", request.url)); // Redirect to homepage
+    }
+    return NextResponse.next(); // Allow access to other routes
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/login/:path*", "/signup/:path*", "/dashboard/:path*"],
+};

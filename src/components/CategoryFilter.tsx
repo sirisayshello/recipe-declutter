2cb2624c24dc160a78c307855e47de0a15efb2fb
sirisayshello <veckovis.salongen0p@icloud.com>
2024-10-28 20:33:57 +0100

    update dashboard, id, categoryFilter to mantine

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -1,74 +1,36 @@
 "use client";
 
-import {
-  Box,
-  Chip,
-  FormControl,
-  FormHelperText,
-  IconButton,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  SelectChangeEvent,
-} from "@mui/material";
-import AddCircleIcon from "@mui/icons-material/AddCircle";
-import React from "react";
+import React, { useState } from "react";
+import { ActionIcon, Group, MultiSelect } from "@mantine/core";
+import { IconCirclePlus } from "@tabler/icons-react";
 
 export default function CategoryFilter({
   categories,
 }: {
   categories: string[];
 }) {
-  const [filteredCategories, setFilteredCategories] = React.useState<string[]>(
-    []
-  );
+  const [filteredCategories, setFilteredCategories] = useState<string[]>([]);
 
-  const handleChange = (
-    event: SelectChangeEvent<typeof filteredCategories>
-  ) => {
-    const {
-      target: { value },
-    } = event;
-    setFilteredCategories(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
-    );
-  };
   return (
-    <>
-      <Box sx={{ display: "flex", alignItems: "flex-start" }}>
-        <FormControl fullWidth>
-          <InputLabel id="recipe-categories">categories</InputLabel>
-          <Select
-            labelId="recipe-categories"
-            id="recipe-categories"
-            multiple
-            value={filteredCategories}
-            label="recipe-categories"
-            onChange={handleChange}
-            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-            renderValue={(selected) => (
-              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
-                {selected.map((value) => (
-                  <Chip key={value} label={value} />
-                ))}
-              </Box>
-            )}
-          >
-            {categories.map((category) => (
-              <MenuItem key={category} value={category}>
-                {category}
-              </MenuItem>
-            ))}
-          </Select>
-          <FormHelperText>Filter by your categories</FormHelperText>
-        </FormControl>
-
-        <IconButton aria-label="add recipe">
-          <AddCircleIcon fontSize="large" />
-        </IconButton>
-      </Box>
-    </>
+    <Group justify="center" align="center" wrap="nowrap">
+      <MultiSelect
+        label="Your recipe lables"
+        placeholder="Pick label"
+        data={categories}
+        value={filteredCategories}
+        onChange={setFilteredCategories}
+        searchable
+        clearable
+        style={{ width: "100%" }}
+      />
+      <ActionIcon
+        variant="transparent"
+        aria-label="Add recipe"
+        size="xl"
+        style={{ alignSelf: "end" }}
+      >
+        <IconCirclePlus style={{ width: "100%", height: "100%" }} stroke={1} />
+      </ActionIcon>
+    </Group>
   );
 }

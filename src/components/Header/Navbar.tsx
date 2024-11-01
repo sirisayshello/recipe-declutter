d5dcf36a000f14fa2bb6e9b5e8aa558efaf76b9f
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-01 15:19:19 +0100

    fix links padding

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Container, Burger, Anchor, Stack } from "@mantine/core";
+import { Container, Burger, Anchor, Stack, Box } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import classes from "./Navbar.module.css";
 import Link from "next/link";
@@ -28,28 +28,32 @@ export const Navbar = () => {
         <LoginButton session={session} />
 
         {opened && (
-          <Stack className={classes.linksContainer}>
-            <Link
-              href="/about"
-              className={classes.link}
-              data-active={active === "/about" ? "true" : undefined}
-              onClick={() => {
-                setActive("/about");
-              }}
-            >
-              About
-            </Link>
-            <a
-              href="https://github.com/sirisayshello/recipe-declutter"
-              className={classes.link}
-              target="_blank"
-              onClick={() => {
-                setActive("https://github.com/sirisayshello/recipe-declutter");
-              }}
-            >
-              Github
-            </a>
-          </Stack>
+          <Box className={classes.linksContainer}>
+            <Container>
+              <Link
+                href="/about"
+                className={classes.link}
+                data-active={active === "/about" ? "true" : undefined}
+                onClick={() => {
+                  setActive("/about");
+                }}
+              >
+                About
+              </Link>
+              <a
+                href="https://github.com/sirisayshello/recipe-declutter"
+                className={classes.link}
+                target="_blank"
+                onClick={() => {
+                  setActive(
+                    "https://github.com/sirisayshello/recipe-declutter"
+                  );
+                }}
+              >
+                Github
+              </a>
+            </Container>
+          </Box>
         )}
       </Container>
     </header>

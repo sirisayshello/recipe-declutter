b425a2c989f51e144c17c301815ef5829f01ecbe
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-01 15:02:07 +0100

    only show recent recipes if there are any

--- src/components/RecentRecipes.tsx
+++ src/components/RecentRecipes.tsx
@@ -1,4 +1,4 @@
-import { Anchor, List, Title } from "@mantine/core";
+import { Anchor, Box, List, Title } from "@mantine/core";
 import Link from "next/link";
 
 export default async function RecentRecipes({
@@ -8,28 +8,32 @@ export default async function RecentRecipes({
 }) {
   return (
     <>
-      <Title order={2}>Recent recipes</Title>
-      <List listStyleType="none">
-        {recipes.map((recipe) => {
-          return (
-            <li
-              key={recipe.id}
-              style={{
-                borderBottom: "1px solid var(--mantine-color-gray-3)",
-                padding: "10px 0",
-              }}
-            >
-              <Anchor
-                component={Link}
-                href={`/dashboard/${recipe.id}`}
-                underline="never"
-              >
-                {recipe.title}
-              </Anchor>
-            </li>
-          );
-        })}
-      </List>
+      {recipes.length > 0 && (
+        <Box component="section">
+          <Title order={2}>Recent recipes</Title>
+          <List listStyleType="none">
+            {recipes.map((recipe) => {
+              return (
+                <li
+                  key={recipe.id}
+                  style={{
+                    borderBottom: "1px solid var(--mantine-color-gray-3)",
+                    padding: "10px 0",
+                  }}
+                >
+                  <Anchor
+                    component={Link}
+                    href={`/dashboard/${recipe.id}`}
+                    underline="never"
+                  >
+                    {recipe.title}
+                  </Anchor>
+                </li>
+              );
+            })}
+          </List>
+        </Box>
+      )}
     </>
   );
 }

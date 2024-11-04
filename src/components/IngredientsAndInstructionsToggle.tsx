9c64e15857892239fc11d4ed021a37f7e7d6fd7b
Julia <jsgoransson@gmail.com>
2024-11-04 14:54:43 +0100

    made instructions rendering into its own component

--- src/components/IngredientsAndInstructionsToggle.tsx
+++ src/components/IngredientsAndInstructionsToggle.tsx
@@ -1,38 +1,11 @@
 "use client";
 import { Button, Group, List, Stack, Text, Title } from "@mantine/core";
 import { useState } from "react";
-import { render } from "react-dom";
+import renderInstructions from "./RenderedInstructions";
 
 type IngAndInstToggleProps = {
   recipe: Recipe;
 };
-
-function renderInstructions(recipe) {
-  if (typeof recipe.instructions[0] === "string") {
-    return (
-      <List type="ordered" spacing="xs">
-        {recipe.instructions.map((instruction, index) => {
-          return <List.Item key={index}>{instruction}</List.Item>;
-        })}
-      </List>
-    );
-  } else if (typeof recipe.instructions[0] === "object") {
-    return (
-      <>
-        {recipe.instructions.map((section, sectionIndex) => (
-          <div key={sectionIndex}>
-            <Title order={3}>{section.name}</Title>
-            <List type="ordered" spacing="xs">
-              {section.text.map((instruction, index) => (
-                <List.Item key={index}>{instruction}</List.Item>
-              ))}
-            </List>
-          </div>
-        ))}
-      </>
-    );
-  }
-}
 
 export const IngredientsAndInstructionsToggle = ({
   recipe,

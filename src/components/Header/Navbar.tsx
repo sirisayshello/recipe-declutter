32f08b04deb00ccd342e581be69bf67d9cbb3169
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-07 10:06:41 +0100

    refactor styling for navbar, enable click outside to close

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,48 +1,68 @@
 "use client";
 
 import { useState } from "react";
-import { Container, Burger, Anchor, Box } from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
-import classes from "./Navbar.module.css";
+import { Container, Burger, Anchor, Text, Paper, Stack } from "@mantine/core";
+import { useClickOutside, useDisclosure } from "@mantine/hooks";
 import Link from "next/link";
 import { LoginButton } from "../LogInButton";
 import { useSession } from "next-auth/react";
 
 export const Navbar = () => {
   const { data: session } = useSession();
-  const [opened, { toggle }] = useDisclosure(false);
+  const [opened, { toggle, close }] = useDisclosure(false);
+  const ref = useClickOutside(() => close());
   const [active, setActive] = useState("/");
 
+  const containerProps = {
+    h: 56,
+    style: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+  };
+
   return (
-    <header className={classes.header}>
-      <Container size="md" className={classes.inner}>
+    <header
+      style={{
+        height: "56px",
+        backgroundColor: "var(--mantine-color-dustyRed-7)",
+      }}
+    >
+      <Container {...containerProps} size="md">
         <Burger color="cream.0" opened={opened} onClick={toggle} size="sm" />
-        <Anchor
-          component={Link}
-          className={classes.link}
-          href="/"
-          underline="never"
-        >
-          Recipe Declutter
+        <Anchor component={Link} href="/" underline="never">
+          <Text c="cream.0" fw={500}>
+            Recipe Declutter
+          </Text>
         </Anchor>
         <LoginButton session={session} />
 
         {opened && (
-          <Box className={classes.linksContainer}>
-            <Container>
-              <Link
+          <Paper
+            ref={ref}
+            pos="absolute"
+            top={56}
+            left={0}
+            right={0}
+            p={16}
+            shadow="md"
+            radius={0}
+          >
+            <Stack pl={6}>
+              <Anchor
+                component={Link}
                 href="/about"
-                className={classes.link}
                 data-active={active === "/about" ? "true" : undefined}
                 onClick={() => {
                   setActive("/about");
                 }}
               >
                 About
-              </Link>
-              <a
+              </Anchor>
+              <Anchor
+                component={Link}
                 href="https://github.com/sirisayshello/recipe-declutter"
-                className={classes.link}
                 target="_blank"
                 onClick={() => {
                   setActive(
@@ -51,9 +71,9 @@ export const Navbar = () => {
                 }}
               >
                 Github
-              </a>
-            </Container>
-          </Box>
+              </Anchor>
+            </Stack>
+          </Paper>
         )}
       </Container>
     </header>

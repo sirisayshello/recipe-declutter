a5394f0a91cd4d5f6cfe0507ceec4200ae969206
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-07 15:22:12 +0100

    add personalized welcome message depending on time of day

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -98,3 +98,17 @@ export function isSimpleInstruction(
     (instructions.length === 0 || typeof instructions[0] === "string")
   );
 }
+
+export const getTimeOfDay = () => {
+  const currentHour = new Date().getHours();
+
+  if (currentHour >= 5 && currentHour < 12) {
+    return "morning";
+  } else if (currentHour >= 12 && currentHour < 18) {
+    return "afternoon";
+  } else if (currentHour >= 18 && currentHour < 22) {
+    return "evening";
+  } else {
+    return "night";
+  }
+};

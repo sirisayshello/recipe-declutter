adef6cb7fff73315ead80dcac703723804a5edf6
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-08 15:04:02 +0100

    rename component

---
+++ src/components/TagsFilter.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import React, { useState } from "react";
+import { ActionIcon, Group, MultiSelect } from "@mantine/core";
+import { IconCirclePlus } from "@tabler/icons-react";
+
+export default function TagsFilter({ tags }: { tags: string[] }) {
+  const [filteredTags, setFilteredTags] = useState<string[]>([]);
+
+  return (
+    <Group justify="center" align="center" wrap="nowrap">
+      <MultiSelect
+        label="Filter recipes"
+        placeholder="Pick tags"
+        data={tags}
+        value={filteredTags}
+        onChange={setFilteredTags}
+        searchable
+        clearable
+        style={{ width: "100%" }}
+      />
+      <ActionIcon
+        variant="transparent"
+        aria-label="Add recipe"
+        size="xl"
+        style={{ alignSelf: "end" }}
+      >
+        <IconCirclePlus style={{ width: "100%", height: "100%" }} stroke={1} />
+      </ActionIcon>
+    </Group>
+  );
+}

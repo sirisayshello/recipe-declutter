7196675ccb4333bdcdc8a75c98c4da4cab3f32a2
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-13 11:14:58 +0100

    fix: close navbar when clicking the X (#194)
    * refactor navbar burger menu
    
    * add logo/name
    
    * update header to use theme primary color
    
    ---------
    
    Co-authored-by: JoarHansson <124060119+JoarHansson@users.noreply.github.com>

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import { useState } from "react";
-import { Container, Burger, Anchor, Text, Paper, Stack } from "@mantine/core";
-import { useClickOutside, useDisclosure } from "@mantine/hooks";
+import {
+  Container,
+  Burger,
+  Anchor,
+  Text,
+  Stack,
+  Drawer,
+  Divider,
+  Group,
+  useMantineTheme,
+  Box,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import { UserButton } from "../LogInButton";
 import UserMenu from "./UserMenu";
+// import { usePathname } from "next/navigation";
 
 export const Navbar = () => {
   const { data: session } = useSession();
-  const [opened, { toggle, close }] = useDisclosure(false);
-  const ref = useClickOutside(() => close());
-  const [active, setActive] = useState("/");
+  const [opened, { open, close }] = useDisclosure(false);
+  const theme = useMantineTheme();
+  // usePathname if we want highlighted links
+  // const active = usePathname();
 
   const containerProps = {
     h: 56,
@@ -24,14 +36,50 @@ export const Navbar = () => {
   };
 
   return (
-    <header
-      style={{
-        height: "56px",
-        backgroundColor: "var(--mantine-color-dustyRed-7)",
-      }}
-    >
+    <Box component="header" bg={theme.primaryColor} h={"56px"}>
       <Container {...containerProps} size="md">
-        <Burger color="cream.0" opened={opened} onClick={toggle} size="sm" />
+        <Burger
+          hiddenFrom="xs"
+          color="cream.0"
+          opened={opened}
+          onClick={open}
+          size="md"
+        />
+        <Drawer opened={opened} onClose={close} withCloseButton={false}>
+          <Group>
+            <Drawer.CloseButton m={0} />
+            <Text size="xl" fw={700}>
+              Recipe Declutter
+            </Text>
+          </Group>
+          <Divider mt={16} mb={16} />
+          <Stack gap={24}>
+            <Anchor component={Link} href="/about">
+              About
+            </Anchor>
+            <Anchor
+              component={Link}
+              href="https://github.com/sirisayshello/recipe-declutter"
+              target="_blank"
+            >
+              Github
+            </Anchor>
+          </Stack>
+        </Drawer>
+
+        <Group visibleFrom="xs">
+          <Anchor c="cream.0" component={Link} href="/about">
+            About
+          </Anchor>
+          <Anchor
+            c="cream.0"
+            component={Link}
+            href="https://github.com/sirisayshello/recipe-declutter"
+            target="_blank"
+          >
+            Github
+          </Anchor>
+        </Group>
 
         <Anchor component={Link} href="/" underline="never">
           <Text c="cream.0" fw={500}>
@@ -48,47 +96,7 @@ export const Navbar = () => {
             displayName={session.user.name?.charAt(0).toUpperCase() || "U"}
           />
         )}
-
-        {opened && (
-          <Paper
-            ref={ref}
-            pos="absolute"
-            top={56}
-            left={0}
-            right={0}
-            p={16}
-            shadow="md"
-            radius={0}
-          >
-            <Container>
-              <Stack pl={6}>
-                <Anchor
-                  component={Link}
-                  href="/about"
-                  data-active={active === "/about" ? "true" : undefined}
-                  onClick={() => {
-                    setActive("/about");
-                  }}
-                >
-                  About
-                </Anchor>
-                <Anchor
-                  component={Link}
-                  href="https://github.com/sirisayshello/recipe-declutter"
-                  target="_blank"
-                  onClick={() => {
-                    setActive(
-                      "https://github.com/sirisayshello/recipe-declutter"
-                    );
-                  }}
-                >
-                  Github
-                </Anchor>
-              </Stack>
-            </Container>
-          </Paper>
-        )}
       </Container>
-    </header>
+    </Box>
   );
 };

9a559dfbb83a0b84de17820b27dbcb62d940cca4
sirisayshello <veckovis.salongen0p@icloud.com>
2024-11-14 11:47:16 +0100

    create DashboardRecipeList component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,9 @@
-import TagsFilter from "@/components/TagsFilter";
-import { Center, Stack, Title } from "@mantine/core";
-import RecipesList from "@/components/RecipesList";
+import { Box, Button, Group, Text, Title } from "@mantine/core";
 import { getAuth } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import SignOutButton from "@/components/SignOutButton";
 import { getRecipesByUserId, getUserTags } from "@/lib/queries";
+import { IconArrowsShuffle2, IconCirclePlus } from "@tabler/icons-react";
+import { DashboardRecipeList } from "@/components/DashboardRecipeList";
 
 export default async function Dashboard() {
   const session = await getAuth();
@@ -14,24 +13,27 @@ export default async function Dashboard() {
   }
 
   const userId = session.user.id;
-
-  // fetch all of the user's recipes for the "Saved Recipes" list
   const recipes = await getRecipesByUserId(userId, "noLimit");
+  const numberOfRecipes = recipes.length;
 
   const convertedRecipes = recipes as UserRecipe[];
   const tags = await getUserTags(session.user.id);
 
   return (
     <>
-      <Center component="section" mt="md">
-        <Title mb="xl">Your Recipes</Title>
-      </Center>
-
-      <Stack component="section">
-        <TagsFilter tags={tags} />
-        <RecipesList recipes={convertedRecipes} title="Saved Recipes" />
-      </Stack>
-      <SignOutButton />
+      <Box mt="md" mb="md" component="section">
+        <Title mb="xs">Your recipes</Title>
+        <Text>
+          You have {numberOfRecipes} delicious recipes in your collection
+        </Text>
+        <Group mt="xl">
+          <Button leftSection={<IconCirclePlus />}>Add New Recipe</Button>
+          <Button variant="light" leftSection={<IconArrowsShuffle2 />}>
+            Random Recipe
+          </Button>
+        </Group>
+      </Box>
+      <DashboardRecipeList tags={tags} recipes={convertedRecipes} />
     </>
   );
 }

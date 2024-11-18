bd8ed108f2483bae490eb019b117dfe94c40a760
juliasophieg <jsgoransson@gmail.com>
2024-11-18 15:07:13 +0100

    confetti size and amount based on screen size

--- src/components/Confetti.tsx
+++ src/components/Confetti.tsx
@@ -4,10 +4,16 @@ import JSConfetti from "js-confetti";
 export const Confetti = () => {
   useEffect(() => {
     const jsConfetti: JSConfetti = new JSConfetti();
+
+    // Determine emoji and confetti size based on screen width
+    const screenWidth = window.innerWidth;
+    const emojiSize = screenWidth < 575 ? 70 : 100;
+    const confettiNumber = screenWidth < 575 ? 15 : 30;
+
     jsConfetti.addConfetti({
-      emojis: ["🥗", "🍳", "🌮", "🌭", "🍔", "🍝 ", "🥞"],
-      emojiSize: 100,
-      confettiNumber: 30,
+      emojis: ["🥗", "🍳", "🌮", "🌭", "🍔", "🍝", "🥞"],
+      emojiSize: emojiSize,
+      confettiNumber: confettiNumber,
     });
   }, []);
 
